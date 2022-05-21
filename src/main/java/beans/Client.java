f0378c6857017f2b50172403f84af80ee9d0f9d1
KarasikKk <bayanist1995@mail.ru>
2022-05-21 14:31:03 +0300

    Task from video 3

--- src/main/java/beans/Client.java
+++ src/main/java/beans/Client.java
@@ -1,6 +1,7 @@
+package beans;
+
 public class Client {
     private String id;
-
     private String fullName;
 
     public Client(String id, String fullName) {
--- src/main/java/loggers/ConsoleEventLogger.java
+++ src/main/java/loggers/ConsoleEventLogger.java
@@ -1,4 +1,6 @@
-public class ConsoleEventLogger implements EventLogger{
+package loggers;
+
+public class ConsoleEventLogger implements EventLogger {
     @Override
     public void logEvent(String msg) {
         System.out.println(msg);
